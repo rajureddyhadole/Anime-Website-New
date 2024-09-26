eade7a3c7e664abacb730b68fa385f3bea8f64d6
Hadole Raju <67407713+rajureddyhadole@users.noreply.github.com>
2024-09-26 19:03:29 +0530

    intial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/src/assets/anime-pfp.jpg" alt="profile-pic" />
+        <span>AnimeGen</span>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
